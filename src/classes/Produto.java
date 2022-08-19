0b3345e43e72380186ec6be19c403fd853f7de06
Saulo Henrique Rodrigues <sauloHrodrigues@gmail.com>
2022-08-19 09:51:15 -0300

    Classe Produto implementada. Falta o metodo "preçoEtiqueta", a ser implementado no decorrer do projeto.

--- src/classes/Produto.java
+++ src/classes/Produto.java
@@ -1,4 +1,38 @@
 package classes;
 
 public class Produto {
+    private String nomeProduto;
+    private Double precoProduto;
+
+    // Contrutores
+    public Produto(){
+    }
+
+    public Produto(String nomeProduto, Double precoProduto) {
+        this.nomeProduto = nomeProduto;
+        this.precoProduto = precoProduto;
+    }
+
+    //Metodos
+
+    // implementar
+
+    //geters e setters
+
+
+    public String getNomeProduto() {
+        return nomeProduto;
+    }
+
+    public void setNomeProduto(String nomeProduto) {
+        this.nomeProduto = nomeProduto;
+    }
+
+    public Double getPrecoProduto() {
+        return precoProduto;
+    }
+
+    public void setPrecoProduto(Double precoProduto) {
+        this.precoProduto = precoProduto;
+    }
 }
